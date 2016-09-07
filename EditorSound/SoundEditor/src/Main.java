974ac3b863d6e1fb8a930706d95d7a13db8c5b88
Cretu Calin <cretucalin06@yahoo.ro>
2016-09-07 22:36:51 +0300

    Update Main.java

--- EditorSound/SoundEditor/src/Main.java
+++ EditorSound/SoundEditor/src/Main.java
@@ -1,18 +1,23 @@
 
-import java.awt.Desktop;
+import java.awt.FileDialog;
 import java.io.File;
 import java.io.IOException;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.swing.JFrame;
+
+    
 	public class Main {
+		   private static String path;
 		   public static void main(String[] args) throws IOException {
-		        Desktop desktop = Desktop.getDesktop();
-		        File dirToOpen = null;
-		        try {
-		            dirToOpen = new File("c:\\");
-		            desktop.open(dirToOpen);
-		        } catch (IllegalArgumentException iae) {
-		            System.out.println("File Not Found");
-		        }
+			   	SelectFile selectFile = new SelectFile() ;
+			   	path = selectFile.getFilePath();
+			   	PlaySound.play(path); // works
 		    }
-		}
+		    
+	}
 
